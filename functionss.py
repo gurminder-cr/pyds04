4ba62a1d37b6811bc60ef35eb05913de80ac8ff0
gurminder-cr <gurminder@coderroots.com>
2026-06-17 16:56:51 +0530

    Functions

---
+++ functionss.py
@@ -0,0 +1,57 @@
+# functions 
+# user defined function & pre-defined functions 
+# pre-defined  - input(), print(), sum(),..... 
+# def -keyword 
+
+def printVal():
+    print("Hello this is a function")
+
+# printVal()
+
+# def func1():
+#     a=int(input("enter a "))
+#     b=int(input("enter b "))
+#     print(a+b)
+
+# def func1():
+#     a=int(input("enter a "))
+#     if a%2==0:
+#         print("Even")
+#     else :
+#         print("Odd")
+# func1()
+        
+# def sumVal(a,b): # parameters
+#     print(a+b)
+    
+# sumVal(10,12)  # arguments
+
+# def sumVal(a,b): # parameters
+#     print(a+b)
+#     return "hello"
+# def sumVal(a,b): # parameters
+#     return a+b
+
+# i=int(input("i "))
+# j=int(input("j "))
+# print(sumVal(i,j))
+
+
+# function with keyword arguments 
+
+# def printall(a,b,c):
+#     print(a,b,c)
+    
+# # i=10
+# # j=15
+# # k=20
+# printall(c=30,b=20,a=10)
+
+
+# def sumList(a):
+#     print(a)
+#     s=0
+#     for i in a:
+#         s+=i  # s=s+i
+#     return s    
+# print(sumList([12,13,15,44,32]))
